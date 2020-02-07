8201f43f178e5e715d3f8d6f00ed2f4540b001c4
Milad E. Fahmy <miladezzat.f@gmail.com>
2020-02-07 15:07:42 +0200

    Create api for delete item

--- routes/api/items.js
+++ routes/api/items.js
@@ -29,6 +29,16 @@ router.post('/', (req, res) => {
         .catch(err => res.json(err))
 });
 
+/**
+ * @route DELETE api/items/:id
+ * @description Delete An Item
+ * @access Public
+ */
+router.delete('/:id', (req, res) => {
+    ItemModel.findById(req.params.id)
+    .then(item=> item.remove().then(()=>  res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}))
+});
 
 
 module.exports = router;
